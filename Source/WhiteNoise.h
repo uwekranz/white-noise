298590527eadbcddd2088a9b2172e24af2486137
Uwe Kranz <uwe.karsten.kranz@gmail.com>
2022-03-27 12:33:39 +0200

    formatting

--- Source/WhiteNoise.h
+++ Source/WhiteNoise.h
@@ -1,12 +1,10 @@
 #pragma once
 
-class MainContentComponent: public AudioAppComponent
-{
+class MainContentComponent : public AudioAppComponent {
 public:
-    MainContentComponent()
-    {
-        setSize (600, 100);
-        setAudioChannels (0, 2);
+    MainContentComponent() {
+        setSize(600, 100);
+        setAudioChannels(0, 2);
 
         addLeftNoiseLevelSlider();
         addRightNoiseLevelSlider();
@@ -22,43 +20,37 @@ public:
     }
 
     void addLeftNoiseLevelSlider() {
-        channel1LevelSlider.setRange (0.0, 0.25);
-        channel1LevelSlider.setTextBoxStyle (Slider::TextBoxRight, false, 100, 20);
-        channel1LevelLabel.setText ("Channel 1 Level", dontSendNotification);
+        channel1LevelSlider.setRange(0.0, 0.25);
+        channel1LevelSlider.setTextBoxStyle(Slider::TextBoxRight, false, 100, 20);
+        channel1LevelLabel.setText("Channel 1 Level", dontSendNotification);
 
         addAndMakeVisible(channel1LevelSlider);
         addAndMakeVisible(channel1LevelLabel);
     }
 
-    ~MainContentComponent() override
-    {
+    ~MainContentComponent() override {
         shutdownAudio();
     }
 
-    void prepareToPlay (int, double) override {}
+    void prepareToPlay(int, double) override {}
 
-    void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override
-    {
-        for (auto channel = 0; channel < bufferToFill.buffer->getNumChannels(); ++channel)
-        {
+    void getNextAudioBlock(const AudioSourceChannelInfo &bufferToFill) override {
+        for (auto channel = 0; channel < bufferToFill.buffer->getNumChannels(); ++channel) {
             float level = 0;
             float levelScale = 0;
 
-            if (channel == 0)
-            {
+            if (channel == 0) {
                 level = (float) channel0LevelSlider.getValue();
                 levelScale = level * 2;
             }
-            if (channel == 1)
-            {
+            if (channel == 1) {
                 level = (float) channel1LevelSlider.getValue();
                 levelScale = level * 2;
             }
 
-            auto* buffer = bufferToFill.buffer->getWritePointer (channel, bufferToFill.startSample);
+            auto *buffer = bufferToFill.buffer->getWritePointer(channel, bufferToFill.startSample);
 
-            for (auto sample = 0; sample < bufferToFill.numSamples; ++sample)
-            {
+            for (auto sample = 0; sample < bufferToFill.numSamples; ++sample) {
                 buffer[sample] = random.nextFloat() * levelScale - level;
             }
         }
@@ -66,23 +58,22 @@ public:
 
     void releaseResources() override {}
 
-    void resized() override
-    {
+    void resized() override {
         channel0LevelLabel.setBounds(10, 10, 90, 20);
         channel0LevelSlider.setBounds(100, 10, getWidth() - 110, 20);
 
-        channel1LevelLabel .setBounds (10, 40, 90, 20);
-        channel1LevelSlider.setBounds (100, 40, getWidth() - 110, 20);
+        channel1LevelLabel.setBounds(10, 40, 90, 20);
+        channel1LevelSlider.setBounds(100, 40, getWidth() - 110, 20);
     }
 
 private:
     Random random;
 
     Slider channel1LevelSlider;
-    Label  channel1LevelLabel;
+    Label channel1LevelLabel;
 
     Slider channel0LevelSlider;
-    Label  channel0LevelLabel;
+    Label channel0LevelLabel;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
 };
